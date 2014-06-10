2d5166fa7cdd4383d84842a6e03ea805fb9a6d68
Corey Norris <coreypnorris@gmail.com>
2014-06-10 15:37:28 -0700

    add some donations for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Donation.create(:name => "Small Donation", :price => 5)
+Donation.create(:name => "Medium Donation", :price => 10)
+Donation.create(:name => "Large Donation", :price => 50)
+Donation.create(:name => "Generous Donation", :price => 100)
